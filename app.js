9ba9fa159681c895a0af078f70ea934ba9d42694
Nicolás Carducci <nicolasc95@gmail.com>
2020-11-08 19:02:36 -0300

    square movement in X

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 var canvas = null;
 var ctx = null;
+var x = 50,
+    y = 50;
 
 function paint (ctx) {
+  ctx.fillStyle = '#000';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
   ctx.fillStyle = '#0f0';
-  ctx.fillRect(100, 50, 100, 100);
+  ctx.fillRect(x, y, 10, 10);
+}
+
+function run() {
+  window.requestAnimationFrame(run);
+  act();
+  paint(ctx);
+}
+
+function act() {
+  x+= 2;
 }
 
 function init() {
   canvas = document.getElementById('canvas');
   ctx = canvas.getContext('2d');
-  paint (ctx);
+  run();
 }
+
+
 
 window.addEventListener('load', init, false);
 
