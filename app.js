1d9da49789be32ae51d79a0e3c05ed205351814f
Nicolás Carducci <nicolasc95@gmail.com>
2020-11-08 22:39:38 -0300

    1.04 Interaction with objects and score

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var canvas = null;
 var ctx = null;
-var x = 50,
-    y = 50;
+var player = null;
 var dir = 0;
 var lastPress = null;
 var pause = true;
+
+var score = 0;
 
 var keyEnter = 13;
 var keyLeft= 37,
@@ -27,23 +28,59 @@ document.addEventListener('keydown', function (evt) {
   lastPress = evt.which;
 }, false);
 
-//Base
+function random(max) {
+  return Math.floor(Math.random() * max);
+  }
+
+//Rectangle
+function Rectangle(x, y, width, height) {
+  this.x = (x == null) ? 0 : x;
+  this.y = (y == null) ? 0 : y;
+  this.width = (width == null) ? 0 : width;
+  this.height = (height == null) ? this.width : height;
+  this.intersects = function (rect) {
+    if (rect == null) {
+      window.console.warn('Missing parameters on function intersects');
+      } else {
+      return (this.x < rect.x + rect.width &&
+      this.x + this.width > rect.x &&
+      this.y < rect.y + rect.height &&
+      this.y + this.height > rect.y);
+    }
+  };
+  this.fill = function (ctx) {
+    if (ctx == null) {
+      window.console.warn('Missing parameters on function fill');
+      } else {
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+  };
+}
+//Paint
 function paint (ctx) {
+  //Canvas
   ctx.fillStyle = '#000';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-
+  //Player 
   ctx.fillStyle = '#c89483';
-  ctx.fillRect(x, y, 10, 10);
+  player.fill(ctx);
+  //Food
+  ctx.fillStyle = '#95a1ac';
+  food.fill(ctx);
+  // Draw score
+  ctx.fillStyle = '#d37c66';
+  ctx.fillText('Score: ' + score, 0, 10);
+  //Last Key pressed
+  //ctx.fillStyle = '#fff';
+  //ctx.fillText('Last Press: ' + lastPress, 0, 20);
 
-  ctx.fillStyle = '#fff';
-  ctx.fillText('Last Press: ' + lastPress, 0, 20);
-
-//Draw pause
-if (pause) {
-  ctx.textAlign = 'center';
-  ctx.fillText('PAUSE', 150, 75);
-  ctx.textAlign = 'left';
-  }
+  //Draw pause
+  if (pause) {
+    ctx.textAlign = 'center';
+    ctx.fillStyle = '#d37c66';
+    ctx.fillText('PAUSE', 150, 75);
+    ctx.textAlign = 'left';
+    }
 }
 
 function act() {
@@ -64,31 +101,37 @@ function act() {
 
     //Move Rect
     if (dir == 0) {
-      y -= 5;
+      player.y -= 5;
     }
     if (dir == 1) {
-      x += 5;
+      player.x += 5;
     }
     if (dir == 2) {
-      y += 5;
+      player.y += 5;
     }
     if (dir == 3) {
-      x -= 5;
+      player.x -= 5;
     }
 
     //Out Screen
-    if (x > canvas.width) {
-      x = 0;
+    if (player.x > canvas.width) {
+      player.x = 0;
     }
-    if (y > canvas.height) {
-      y = 0;
+    if (player.y > canvas.height) {
+      player.y = 0;
     }
-    if (x < 0) {
-      x = canvas.width;
+    if (player.x < 0) {
+      player.x = canvas.width;
     }
-    if (y < 0) {
-      y = canvas.height;
+    if (player.y < 0) {
+      player.y = canvas.height;
     }
+    // Food Intersects
+if (player.intersects(food)) {
+  score += 1;
+  food.x = random(canvas.width / 10 - 1) * 10;
+  food.y = random(canvas.height / 10 - 1) * 10;
+  }
   }
 
 // Pause/Unpause
@@ -113,6 +156,9 @@ function run () {
 function init() {
   canvas = document.getElementById('canvas');
   ctx = canvas.getContext('2d');
+  //Player
+  player = new Rectangle(40, 40, 10, 10);
+  food = new Rectangle(80, 80, 10, 10)
   run();
   repaint();
 }
